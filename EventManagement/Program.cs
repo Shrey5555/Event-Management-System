556dfca2bf22a5c7ae79a2aec6967195e8ce18de
Shrey5555 <shreygupta0509@gmail.com>
2025-02-19 01:07:58 +0530

    Docker updates

--- EventManagement/Program.cs
+++ EventManagement/Program.cs
@@ -6,26 +6,30 @@ using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using EventManagement.Models;
 using EventManagement;
+
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("EventManagementContextConnection") ?? throw new InvalidOperationException("Connection string 'EventManagementContextConnection' not found.");
+
+var connectionString = builder.Configuration.GetConnectionString("EventManagementContextConnection")
+    ?? throw new InvalidOperationException("Connection string 'EventManagementContextConnection' not found.");
 
 builder.Services.AddDbContext<EventManagementContext>(options =>
-    options.UseSqlServer(connectionString)
-           .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information)); 
+    options.UseNpgsql(connectionString) 
+           .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information));
 
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<EventManagementContext>();
+
 builder.Services.Configure<IdentityOptions>(options =>
 {
     options.ClaimsIdentity.UserIdClaimType = ClaimTypes.NameIdentifier;
-    //options.Password.RequiredLength = 8;
     options.User.RequireUniqueEmail = true;
-
 });
 
-builder.Services.AddMvc().AddViewOptions(options=>options.HtmlHelperOptions.ClientValidationEnabled= true);
+builder.Services.AddMvc().AddViewOptions(options => options.HtmlHelperOptions.ClientValidationEnabled = true);
+
 builder.Services.ConfigureApplicationCookie(options =>
 {
     options.AccessDeniedPath = "/NewHome/AccessDenied";
@@ -42,7 +46,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -50,14 +53,13 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-app.UseAuthentication();;
+app.UseAuthentication(); 
 
 app.UseAuthorization();
 
-    app.MapControllerRoute(
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=NewHome}/{action=Index}/{id?}");
-
 
 app.MapRazorPages();
 app.Run();
